062c5db7432e383853df2f4637bfbe4979e6be67
Yoki Yuqing <yyq2983026@hotmail.com>
2016-04-04 11:07:42 -0400

    Added files via upload

---
+++ LL206ReverseLinkedList.java
@@ -0,0 +1,46 @@
+package happynumber;
+
+
+public class LL206ReverseLinkedList {
+	public static class ListNode {
+		 int val;
+		 ListNode next;
+		 ListNode(int x) { val = x; }
+	}
+	public static ListNode reverseList(ListNode head) {
+		if (head == null) return null;
+		//Always point to the start of the remaining unchanged list
+        ListNode front = head; 
+        //point to the element gonna change its position
+        ListNode rear = head.next; 
+        while (rear != null) {
+        	front.next = rear.next;
+        	rear.next = head;
+        	head = rear;       	
+        	rear = front.next;
+        }
+        return head;
+        
+        //recursive method:
+        
+    }
+	
+	public static void main (String[] args) {
+		ListNode a = new ListNode(1);
+//		ListNode b = new ListNode(2);
+//		ListNode c = new ListNode(3);
+//		ListNode d = new ListNode(4);
+//		ListNode e = new ListNode(5);
+//		a.next = b;
+//		b.next = c;
+//		c.next = d;
+//		d.next = e;e.next = null;
+		a.next = null;
+		ListNode x = reverseList(a);
+		while (x != null) {
+			System.out.println(x.val);
+			x = x.next;
+		}
+		
+	}
+}
