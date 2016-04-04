353914b74ed14cde3ba3e9cb3c3f1f100175b65f
Yoki Yuqing <yyq2983026@hotmail.com>
2016-04-04 00:02:39 -0400

    Added files via upload

---
+++ Array119PascalsTriangle2.java
@@ -0,0 +1,33 @@
+package happynumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array119PascalsTriangle2 {
+	public static List<Integer> getRow(int rowIndex) {
+        List<Integer> prev = new ArrayList<Integer>(); //previous row
+        List<Integer> curr = new ArrayList<Integer>(); //current row
+        for (int i = 0; i <= rowIndex; i++) {
+        	for (int j = 0; j <= i; j++) {//build the current row
+        		if (j == 0 || j == i) {
+        			curr.add(1);
+        		} else {
+        			curr.add(prev.get(j) + prev.get(j - 1));
+        		}
+        	}
+        	if (i != rowIndex) {//build the previous row
+        		prev = curr;
+        		curr = new ArrayList<Integer>();;
+        	} else {
+        		break;
+        	}
+        }
+        return curr;
+    }
+	public static void main (String[] args) {
+		List<Integer> result = new ArrayList<Integer>();
+		result = getRow(3);
+		for (int x : result)
+			System.out.print(x + " ");
+	}
+}
