84e593c668093951afa808a9490a56c328069e19
Yoki <yyq2983026@hotmail.com>
2016-09-06 15:18:01 -0400

    run the MirrorTree() method and make sure to errors

--- Tree/Tree101SymmetricTree.java
+++ Tree/Tree101SymmetricTree.java
@@ -1,22 +1,45 @@
 package Tree;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * Created by XiaoMi on 2016/9/5.
  */
 public class Tree101SymmetricTree {
+    //Runtime: 6ms Use: 20min
     public boolean isSymmetricBySameTree(TreeNode root) {
+        if (root == null) return true;
         TreeNode leftHalfSymmed = MirrorTree(root.left);
-        return Tree100SameTree.isSameTreeByIteration(leftHalfSymmed, root.right);
+        TreeNode p = leftHalfSymmed;
+        TreeNode q = root.right;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if (p != null) { stack.push(p); }
+        if (q != null) { stack.push(q); }
+
+        while (!stack.isEmpty() && stack.size()%2 == 0) {
+            TreeNode pn = stack.pop();
+            TreeNode qn = stack.pop();
+            if (pn.val != qn.val) return false;
+            if (pn.right != null) stack.push(pn.right);
+            if (qn.right != null) stack.push(qn.right);
+            if (stack.size()%2 != 0) return false;
+            if (pn.left != null) stack.push(pn.left);
+            if (qn.left != null) stack.push(qn.left);
+            if (stack.size()%2 != 0) return false;
+        }
+        if (stack.size()%2 != 0) { return false; }
+        else { return true; }
     }
     //Use: 10min
     private static TreeNode MirrorTree (TreeNode root) {
         LinkedList<TreeNode> lltree = new LinkedList<TreeNode>();
         lltree.add(root);
         while (lltree.size() > 0) {
-            if (lltree.peek() == null) {
+            while (lltree.peek() == null) {
                 lltree.remove();
+                if (lltree.size() == 0)
+                    return root;
             }
             TreeNode temp = lltree.peek().left;
             lltree.peek().left = lltree.peek().right;
@@ -54,5 +77,16 @@ public class Tree101SymmetricTree {
         if(!llLeft.isEmpty() || !llRight.isEmpty())
             return false;
         return true;
+    }
+    public static void main(String[] args) {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        a.left = b;
+        a.right = c;
+
+        a = MirrorTree(a);
+        System.out.println(a.left.val);
     }
 }
