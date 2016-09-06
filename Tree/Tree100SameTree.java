d7cbed2a80925c5a3a2dcced589e3005f13636e5
Yoki <yyq2983026@hotmail.com>
2016-09-06 15:16:50 -0400

    tried the DFS by one stack

--- Tree/Tree100SameTree.java
+++ Tree/Tree100SameTree.java
@@ -1,10 +1,8 @@
 package Tree;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
-/**
- * Created by XiaoMi on 2016/9/5.
- */
 public class Tree100SameTree {
     //Runtime: 1ms Use: 20min 9/5/2016 First time do tree problem...Pass at the first time! "BFS"
     public static boolean isSameTreeByBFS (TreeNode p, TreeNode q) {
@@ -42,5 +40,32 @@ public class Tree100SameTree {
         return p.val == q.val
                 && isSameTreeByIteration(p.left, q.left)
                 && isSameTreeByIteration(p.right, q.right);
+    }
+    //runtime: 1ms Use: 0.5hr 9/6/2016
+    //"Can use two stacks as BFS, but can use only one stack also
+    public static boolean isSameTreeByDFS (TreeNode p, TreeNode q) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if (p != null) { stack.push(p); }
+        if (q != null) { stack.push(q); }
+
+        while (!stack.isEmpty() && stack.size()%2 == 0) {
+            TreeNode pn = stack.pop();
+            TreeNode qn = stack.pop();
+            if (pn.val != qn.val) return false;
+            if (pn.right != null) stack.push(pn.right);
+            if (qn.right != null) stack.push(qn.right);
+            if (stack.size()%2 != 0) return false;
+            if (pn.left != null) stack.push(pn.left);
+            if (qn.left != null) stack.push(qn.left);
+            if (stack.size()%2 != 0) return false;
+        }
+        if (stack.size()%2 != 0) { return false; }
+        else { return true; }
+
+    }
+    public static void main(String[] args) {
+        TreeNode p = null;
+        TreeNode q = null;
+        System.out.println(isSameTreeByDFS(p, q));
     }
 }
