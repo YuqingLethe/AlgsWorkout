42af60d9f69ec5e26db7ff5cd02ebe3651b93b42
Yoki <yyq2983026@hotmail.com>
2016-09-29 00:46:45 -0400

    Review some details.

--- HashMap/HM136SingleNumber.java
+++ HashMap/HM136SingleNumber.java
@@ -4,17 +4,17 @@ import java.util.LinkedList;
 
 public class HM136SingleNumber {
     //Time Exceeded 0/28/2016
-    public static int singleNumberByMemory(int[] nums) {
+    private static int singleNumberByMemory(int[] nums) {
         boolean[] code = new boolean[nums.length]; //Has found the duplicate or not
         for (int i = 0; i < nums.length; i++) {
-            if (code[i] == true) { continue; }
+            if (code[i]) { continue; }
             for (int j = i + 1; j < nums.length; j++) {
-                if (code[j] == false && nums[i] == nums[j]) {
+                if (!code[j]  && nums[i] == nums[j]) {
                     code[i] = true;
                     code[j] = true;
                 }
             }
-            if (code[i] == false) {
+            if (!code[i]) {
                 return nums[i];
             }
         }
