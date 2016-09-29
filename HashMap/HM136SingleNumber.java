a5142d058523ef7de52424d07f860afc4124878a
Yoki <yyq2983026@hotmail.com>
2016-09-29 00:45:23 -0400

    The ByArray() function is not linear time. ByLoop() have to use single direction linked list.

---
+++ HashMap/HM136SingleNumber.java
@@ -0,0 +1,29 @@
+package HashMap;
+
+import java.util.LinkedList;
+
+public class HM136SingleNumber {
+    //Time Exceeded 0/28/2016
+    public static int singleNumberByMemory(int[] nums) {
+        boolean[] code = new boolean[nums.length]; //Has found the duplicate or not
+        for (int i = 0; i < nums.length; i++) {
+            if (code[i] == true) { continue; }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (code[j] == false && nums[i] == nums[j]) {
+                    code[i] = true;
+                    code[j] = true;
+                }
+            }
+            if (code[i] == false) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] x = {3,3,1,2,1,4,4,2,6};
+        System.out.println(singleNumberByMemory(x));
+    }
+}
