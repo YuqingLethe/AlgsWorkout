3b61792422b574501bc22cc959a6cdfb97e40202
yuqingLethe <yyq2983026@hotmail.com>
2016-10-22 15:47:38 -0400

    .

---
+++ Math/Math9PalindromeNumber.java
@@ -0,0 +1,46 @@
+package Math;
+
+/**
+ * Runtime: 191ms   10/22/2016中心思想是，v储存后半部分，x储存前半部分，
+ * 比如12321在while循环之后v=123 x=12
+ * 如果位数为奇数就会落到第二个if(v>x)这里从而把中间那个数去掉，所以v=12
+ * https://discuss.leetcode.com/topic/40845/9-ms-java-beats-99-5-java-solutions-easy-to-understand/2
+ * 巧妙在用了一个循环把后半部分的数倒过来
+ */
+public class Math9PalindromeNumber {
+    public boolean isPalindromeGetLatterHalf(int x) {
+        if (x < 0) return false;
+        if(x<10) return true;
+        if(x%10==0) return false;
+        if(x<100&&x%11==0) return true;
+        if(x<1000&&((x/100)*10+x%10)%11==0) return true;
+
+        int v;
+
+        //actual logic
+        v=x%10;
+        x=x/10;
+        while(x-v>0)
+        {
+            v=v*10+x%10;
+            x/=10;
+        }
+        if(v>x){v/=10;}
+        return v==x?true:false;
+    }
+
+    /**
+     * Runtime:    Use:     10/22/2016
+     * To avoid overflow of reversed number, leave the first digit
+     * https://discuss.leetcode.com/topic/9477/o-1-space-o-lgn-time-java-solution-no-overflow-risk/2
+     */
+    public boolean isPalindromeGetReverseNumber(int x) {
+        if (x < 0) return false;
+        int r = 0; //reversed number
+        int o = x; //original number
+
+        while(o > 9) {
+
+        }
+    }
+}
