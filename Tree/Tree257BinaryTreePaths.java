3e9114dc4113a99042f1b8507459d0e1609463fd
YuqingLethe <yyq2983026@hotmail.com>
2016-11-04 20:24:27 -0400

    .

--- Tree/Tree257BinaryTreePaths.java
+++ Tree/Tree257BinaryTreePaths.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Tree257BinaryTreePaths {
     /**
-     * Runtime: 7ms
+     * Runtime: 7ms   11/4/2016
      * Use a flag to node if parent node has two children
      */
     public List<String> binaryTreePathsByIteration(TreeNode root) {
@@ -52,6 +52,46 @@ public class Tree257BinaryTreePaths {
         }
         return result;
     }
+
+    /**
+     * Runtime:  11/4/2016
+     * Use recursive, and optimized String building way
+     * https://discuss.leetcode.com/topic/21474/accepted-java-simple-solution-in-8-lines
+     */
+    public static List<String> binaryTreePathsByRecursive(TreeNode root) {
+        List<String> answer = new ArrayList<>();
+        if (root != null) searchBT(root, "", answer);
+        return answer;
+    }
+    private static void searchBT(TreeNode root, String path, List<String> answer) {
+        if (root.left == null && root.right == null) answer.add(path + root.val);
+        if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
+        if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
+    }
+
+    /**
+     * Runtime:
+     * Optimize recursive version by using only itself
+     * https://discuss.leetcode.com/topic/23047/clean-java-solution-accepted-without-any-helper-recursive-function/2
+     */
+    public List<String> binaryTreePathsByRecursive2(TreeNode root) {
+        List<String> answer = new LinkedList<>();
+        if (root == null) return answer;
+        if (root.left == null && root.right == null) {
+            answer.add(root.val + "");
+            return answer;
+        }
+        for (String path : binaryTreePathsByRecursive2(root.left)) {
+            answer.add(root.val + "->" + path);
+        }
+        for (String path : binaryTreePathsByRecursive2(root.right)) {
+            answer.add(root.val + "->" + path);
+        }
+        return answer;
+    }
+
+    //TODO: USing string builder
+    //https://discuss.leetcode.com/topic/23114/java-solution-using-stringbuilder-instead-of-string-manipulation
     public static void main(String[] args) {
         TreeNode tn1 = new TreeNode(1);
         TreeNode tn2 = new TreeNode(1);
