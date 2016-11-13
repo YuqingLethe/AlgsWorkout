c0fc35d131cecaee0fc74a0de3bca9e309642318
YuqingLethe <yyq2983026@hotmail.com>
2016-11-12 21:29:17 -0500

    .

--- HashMap/HT299BullsAndCows.java
+++ HashMap/HT299BullsAndCows.java
@@ -7,7 +7,7 @@ import java.util.*;
  */
 public class HT299BullsAndCows {
     /**
-     * Runtime: 52ms  Use: 10min
+     * Runtime: 52ms  Use: 10min  11/12/2016
      */
     public static String getHintByArray(String secret, String guess) {
         char[] sarr = secret.toCharArray();
@@ -40,68 +40,105 @@ public class HT299BullsAndCows {
     }
 
     /**
-     * Runtime:  Use:
+     * Runtime: 200ms Use: 1.25 hr for false methods and 5 min for the right methods  11/12/2016
      * Put secret in HashMap, and guess in LinkedLIst
      * Find the bulls and delete them in both HashMap and HashSet
      * Cannot use same method fow cows, because cannot remove element in hashMap by values. Must by keys
      *
      */
-    public static String getHintByMapAndLinkedList(String secret, String guess) {
-        HashMap<Integer, Character> map = new HashMap<>();
-        LinkedList<Character> guessArr = new LinkedList<Character>();
+    public static String getHintByMapsIndexAsKey(String secret, String guess) {
+        HashMap<Integer, Character> smap = new HashMap<>();
+        HashMap<Integer, Character> gmap = new HashMap<>();
         for (Integer i = 0; i < secret.length(); i++) {
-            map.put(i, secret.charAt(i));
+            smap.put(i, secret.charAt(i));
         }
-        for (char c : guess.toCharArray()) {
-            guessArr.add(c);
+        for (Integer i = 0; i < secret.length(); i++) {
+            gmap.put(i, guess.charAt(i));
         }
-        int A = 0, B = 0;
-        //Calculate Bulls
-        Integer k = 0; //index of guessArr
-        while (k < guess.length()) {
-            if (map.get(k) == null) System.out.println("ERROR");
-            if (map.get(k) == guessArr.get(k)) {
+        int A = 0, B = 0; //number of bulls and cows
+        //Get Bulls
+        for (int i = 0; i < secret.length(); i++) {
+            if (smap.get(i) == gmap.get(i)) {
                 A++;
-                map.remove(k);
-                guessArr.remove(k);
-                System.out.println("REMOVE " + k);
-            } else {
-                k++;
+                smap.remove(i);
+                gmap.remove(i);
             }
         }
-        //Calculate Cows. In order to remove the element, have to iterate map to find the key.
-        int i = 0; //index of guessArr
-        while (i < guessArr.size()) {
-            Character c = guessArr.get(i);
-            if (map.containsValue(c)) {
-                B++;
+
+        //Calculate Cows.
+        for (int i = 0; i < secret.length(); i++) {
+            if (smap.containsKey(i)) {
+                Character c = smap.get(i);
                 for (int j = 0; j < secret.length(); j++) {//remove the element from map by the value c
-                    if (map.containsKey(j) && map.get(j) == c) {
-                        map.remove(j);
+                    if (gmap.containsKey(j) && gmap.get(j) == c) {
+                        B++;
+                        gmap.remove(j);
+                        smap.remove(i);
+                        break;
                     }
                 }
             }
-            i++;
         }
         return A + "A" + B + "B";
     }
 
     /**
-     * RUntime:
+     * Runtime: 3ms  Use: 8min  11/12/2016
      * By making a int[10] to store frequency of numbers, we can save one pass
      * https://discuss.leetcode.com/topic/38600/my-3ms-java-solution-may-help-u
      */
     public static String getHintByFrequency(String secret, String guess) {
         int A = 0, B = 0;
+        int[] secretFreq = new int[10];
+        int[] guessFreq = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) {
+                A++;
+            } else {
+                secretFreq[secret.charAt(i) - '0']++;
+                guessFreq[guess.charAt(i) - '0']++;
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            B += Math.min(secretFreq[i], guessFreq[i]);
+        }
         return A + "A" + B + "B";
+    }
+
+    /**
+     * Runtime: 15ms  Use:  20min
+     * Main Idea: B = ALL - A  from Stan?  Use one HashMap for frequency
+     * My Post: https://discuss.leetcode.com/topic/67559/b-all-a-hashmap-one-pass-java-solution-with-explanation
+     */
+    public static String getHintByMapsFrequencyAsValue(String secret, String guess) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : secret.toCharArray()) { //build the map
+            map.put(c, map.containsKey(c) ? map.get(c) + 1 : 1);
+        }
+
+        int A = 0, all = 0; //number of bulls and total number of both-side existing chars
+        for (int i = 0; i < guess.length(); i++) {
+            Character c = guess.charAt(i);
+            //Increment of bulls
+            if (c == secret.charAt(i)) { //Attention: char to Character
+                A++;
+            }
+            //Calculate the total frequency of all appearance of existing chars in guess (ALL)
+            if (map.containsKey(c)) {
+                all++;
+                if (map.get(c) - 1 == 0) map.remove(c);
+                else map.put(c, map.get(c) - 1);
+            }
+        }
+        return A + "A" + (all - A) + "B";
     }
 
     public static void main(String[] args) {
         char c = '4';
         char d = '4';
         System.out.println(c == d);
-        String secret = "1234";
-        String guess = "1532";
-        System.out.println(getHintByMapAndLinkedList(secret, guess));
+        String secret = "1110";
+        String guess =  "0111";
+        System.out.println(getHintByMapsFrequencyAsValue(secret, guess));
     }
 }
--- LinkedList/LL160IntersectionOfTwoLinkedLists.java
+++ LinkedList/LL160IntersectionOfTwoLinkedLists.java
@@ -1,25 +1,118 @@
 package LinkedList;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class LL160IntersectionOfTwoLinkedLists {
-	public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode currA = headA;
-        ListNode currB = headB;
-        while (currA != null) {
-        	while (currB != null) {
-        		if (currA == currB)       	
-        			return currA;
-        		currB = currB.next;
-        	}
-        	currA = currA.next;
-        }
-        return headA;
-    }
-	
-	public static class ListNode {
-		 int val;
-		 ListNode next;
-		 ListNode(int x) { val = x; }
+
+	/**
+	 * 11/12/2016
+	 * Dummy method, time exceed
+	 */
+	public static ListNode getIntersectionNodeTwoLoops(ListNode headA, ListNode headB) {
+		ListNode ai = headA; //pointers of list A
+		while (ai != null) {
+			ListNode bi = headB; //pointer of list B
+			while(bi != null) {
+				if (ai == bi) return ai;
+				bi = bi.next;
+			}
+			ai = ai.next;
+		}
+		return null;
 	}
+
+	/**
+	 * After built the reversed lists, cannot return origial nodes
+	 */
+	public static ListNode getIntersectionNodeReverseList(ListNode headA, ListNode headB) {
+		//Build the reverse list of listB. rpB is at the end of the list B
+		ListNode pointerB = headB;
+		Stack<Integer> stackB = new Stack<>();
+		while(pointerB != null) {
+			stackB.push(pointerB.val);
+		}
+		ListNode reverseB = new ListNode(stackB.pop());
+		ListNode rpB = reverseB;
+		while (!stackB.isEmpty()) {
+			int tmp = stackB.pop();
+			ListNode tmpLL = new ListNode(tmp);
+			rpB.next = tmpLL;
+			rpB = tmpLL;
+		}
+
+		//Build the reverse list of listA. rpA is at the end of the list A
+		ListNode pointerA = headA;
+		Stack<Integer> stackA = new Stack<>();
+		while(pointerB != null) {
+			stackA.push(pointerB.val);
+		}
+		ListNode reverseA = new ListNode(stackA.pop());
+		ListNode rpA = reverseA;
+		while (!stackA.isEmpty()) {
+			int tmp = stackA.pop();
+			rpA.next = new ListNode(tmp);
+			rpA = rpA.next;
+		}
+
+		//Find the intersection
+		while(rpA.next == rpB.next) {
+			rpA = rpA.next;
+			if (rpA != rpB.next) return rpB;
+			else rpB = rpB.next;
+		}
+		return null;
+	}
+
+	/**
+	 * Runtime: 2ms 11/12/2016  Use: 60min
+	 */
+	public static ListNode getIntersectionNodeByLength(ListNode headA, ListNode headB) {
+		ListNode pa = headA, pb = headB;
+		if (pa == null || pb == null) return null;
+		if (pa == pb) return pa; //redundant
+		while(pa != null && pb!= null) {
+			pa = pa.next;
+			pb = pb.next;
+		}
+		ListNode pshort, plong;
+		if (pa == null && pb == null) { //same length
+			pshort = headA;
+			plong = headB;
+		} else if (pa == null) {//List A is shorter
+			pa = headB;
+			while(pb.next != null) {//Stop at the last element of ListB, instead of the null
+				pb = pb.next;
+				pa = pa.next;
+			}
+			plong = pa.next;
+			pshort = headA;
+		} else { //if (pb == null)
+			pb = headA;
+			while(pa.next != null) {
+				pa = pa.next;
+				pb = pb.next;
+			}
+			plong = pb.next;
+			pshort = headB;
+		}
+		if (plong == pshort) return plong; //Don't forget this
+		while(plong != pshort && plong.next != null) {
+			if(plong.next == pshort.next) return plong.next;
+			plong = plong.next;
+			pshort = pshort.next;
+		}
+		return null;
+	}
+
+	/**
+	 * TODO: optimize above one
+	 * https://discuss.leetcode.com/topic/33291/recommend-for-beginners-clean-c-implementation-with-detailed-explaination
+	 */
+	public static ListNode getIntersectionNodeByLength2(ListNode headA, ListNode headB) {
+		return null;
+	}
+
 	
 	public static void main (String[] args) {
 		ListNode a = new ListNode(1);
@@ -27,17 +120,14 @@ public class LL160IntersectionOfTwoLinkedLists {
 		ListNode c = new ListNode(3);
 		ListNode d = new ListNode(4);
 		ListNode e = new ListNode(5);
-		a.next = c;
+		a.next = null;
 		b.next = c;
 		c.next = d;
 		d.next = e;
 		e.next = null;
-		ListNode x = getIntersectionNode(a, b);
-		if (a.next == b.next) {
-			System.out.println("They same");
-		}
+		ListNode x = getIntersectionNodeByLength(e, b);
 		while (x != null) {
-			System.out.println(x.val);
+			System.out.print(" " + x.val);
 			x = x.next;
 		}		
 	}
