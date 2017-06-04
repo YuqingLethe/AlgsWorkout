7e07a043fb7d377a52804fb013cf96864ff34c28
YuqingLethe <yyq2983026@hotmail.com>
2017-06-03 20:41:10 -0400

    .

---
+++ BinarySearch/Binary378KthSmallestElementInASortedArray.java
@@ -0,0 +1,10 @@
+package BinarySearch;
+
+/**
+ * Created by Administrator on 2017/4/2.
+ */
+public class Binary378KthSmallestElementInASortedArray {
+    public int kthSmallest(int[][] matrix, int k) {
+        return 1;
+    }
+}
