0adc471d20d1d6cc4cf11087538d352c7495bbe1
YuqingLethe <yyq2983026@hotmail.com>
2017-06-12 15:35:56 -0400

    Comments move from CSDN

--- Tree/Tree104MaximumDepthOfBinaryTree.java
+++ Tree/Tree104MaximumDepthOfBinaryTree.java
@@ -8,14 +8,26 @@ import java.util.Stack;
  * Created by XiaoMi on 2016/9/7.
  */
 public class Tree104MaximumDepthOfBinaryTree {
-    //Rumtime: 1ms  9/11/2016
+
+    /**
+     * Rumtime: 1ms  9/11/2016
+     * recursive比较好写，但不是自己想的，是看的CC150 Tree Q1
+     */
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
-    //Runtime: 4ms Use: 20min Iterative BFS / level-order 9/11/2016
-    //https://discuss.leetcode.com/topic/27017/clean-java-iterative-solution
+    /**
+     * Runtime: 4ms Use: 20min Iterative BFS / level-order 9/11/2016
+     //https://discuss.leetcode.com/topic/27017/clean-java-iterative-solution
+
+     iterative中的BFS/ level-order traversal
+     主要就是不知道enqueue后左右两个子树比较大小怎么比较。看了discussion一个人分享的solution知道了。
+     1. pop完当前level的所有节点，再Count++. 因此，
+     2. ——>需要在每次count之前，储存需要pop的个数就好了
+     3. 同理Tree102BinaryTreeLevelOrderTraversal也应该这么处理
+     */
     public int maxDepthByBFS(TreeNode root) {
         if (root == null) {
             return 0;
@@ -25,7 +37,7 @@ public class Tree104MaximumDepthOfBinaryTree {
         q.add(root);
         while (!q.isEmpty()) {
             int size = q.size();
-            while (size-- > 0) {
+            while (size-- > 0) { //while (size-- > 0) {}这样处理可以减少循环末尾自己写的size--；之类的东西. 在一个以index或者size为主要循环条件的时候可以用。
                 TreeNode temp = q.remove();
                 if (temp.left != null) { q.add(temp.left); }
                 if (temp.right != null) { q.add(temp.right); }
@@ -34,9 +46,18 @@ public class Tree104MaximumDepthOfBinaryTree {
         }
         return count;
     }
+    /**
+     * Runtime: 11ms    Use: 15min      IterativeDFS 9/11/2016
+     * 不知道一个分支的depth换成另一个分支，还要count–然后再count++这样,怎么知道这是哪一层的呢？
+     看了DFSsolution，有个人用的两个stack，难道是预设一个count比这个大的更新count吗？但即便如此，两个stack分右子树也不够用啊。依旧是分支问题
 
-    //Runtime: 11ms    Use: 15min      IterativeDFS 9/11/2016
-    //https://discuss.leetcode.com/topic/33826/two-java-iterative-solution-dfs-and-bfs
+     * DFS: https://discuss.leetcode.com/topic/33826/two-java-iterative-solution-dfs-and-bfs
+     1. 用两个stack做DFS，一个负责treeNode，另一个负责记录value
+     2. 这样，所有treenodeStack的动作，value都会做一遍
+     3. 设一个max作为最大的maxDepth，不停的与value值比较
+     * @param root
+     * @return
+     */
     public int maxDepthByDFS(TreeNode root) {
         if (root == null) {  return 0; }
         Stack<TreeNode> s = new Stack<TreeNode>();
@@ -52,6 +73,7 @@ public class Tree104MaximumDepthOfBinaryTree {
             if (currTN.left != null) {
                 s.push(currTN.left);
                 value.push (currValue + 1);
+                //这个value stack中，两个子节点和value一起push，但value不可用++currValue因为不能改变自己的值，另一个节点还得用呢！只能currValue + 1这样
             }
             if (currTN.right != null) {
                 s.push(currTN.right);
@@ -59,5 +81,59 @@ public class Tree104MaximumDepthOfBinaryTree {
             }
         }
         return maxDepth;
+    }
+
+    /**
+     * 6/12/2017
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int maxDepthIterative(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int count = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        TreeNode x = new TreeNode(-1);
+        q.add(x);
+        while (q.size() != 0) {
+            TreeNode tmp = q.poll();
+            if (tmp == x)  {
+                count++;
+                if (q.peek() == null) { //在增加x之前要先判断是不是stack的末尾了, 否则容易陷入死循环
+                    break;
+                }
+                q.add(x);
+            } else {
+                if (tmp.left != null) {
+                    q.add(tmp.left);
+                }
+                if (tmp.right != null) {
+                    q.add(tmp.right);
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 6/12/2017 九章答案版本
+     */
+    int dep; //在哪里初始化别忘了
+    public int maxDepthTraverse(TreeNode root) {
+        dep = 0;
+        helper(root, 1);
+        return dep;
+    }
+    private void helper(TreeNode root, int currDepth) {
+        if (root == null) {
+            return;
+        }
+        if (currDepth > dep) {
+            dep = currDepth;
+        }
+        helper(root.left, currDepth + 1);
+        helper(root.right, currDepth + 1);
     }
 }
--- Tree/Tree111MinimumDepthOfBinaryTree.java
+++ Tree/Tree111MinimumDepthOfBinaryTree.java
@@ -4,18 +4,39 @@ import java.util.ArrayDeque;
 import java.util.Stack;
 import java.util.Queue;
 
+/**
+ * 题目要求，一定要到了leaf才算，因此root比较特殊
+ * 数据结构: ArrayList does not implement Queue, but we can use LinkedListor ArrayDeque instead. and ArrayDeque is faster.
+ */
 public class Tree111MinimumDepthOfBinaryTree {
     //Runtime: 1ms Use: 50min 9/11/2016
     public static int minDepthByRecursive(TreeNode root) {
-        if (root == null) { return 0; }
-        if (root.left == null && root.right != null) { return minDepthByRecursive(root.right) + 1; }
-        if (root.left != null && root.right == null) { return minDepthByRecursive(root.left) + 1; }
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right != null) {
+            return minDepthByRecursive(root.right) + 1;
+        }
+        if (root.left != null && root.right == null) {
+            return minDepthByRecursive(root.left) + 1;
+        }
         return Math.min(minDepthByRecursive(root.left), minDepthByRecursive(root.right)) + 1;
         //Lots of optimizations: https://discuss.leetcode.com/topic/8723/my-4-line-java-solution/4
     }
-    //Runtime: 12ms Use: 30min 9/12/2016 stack处理null还是很方便的
+    /**
+     * Runtime: 12ms Use: 30min 9/12/2016 stack处理null还是很方便的
+     * * Tricks 1. 找最大值最小值问题的初始值
+
+     Deal with the first depth as minDepth. See minDepthByBFS()
+     最小值和最大值略不同。找最大值的时候随便初始化一个0中就会被代替，但是最小值只能找第一个有效值来赋值，只能初始化一个无效值。
+     上述的逻辑有问题。他们都必须初始化一个无效值，或者一个小于所有possibilites的值。只不过maximum比较好找，因为tree的个数最小为0.但minimum不好找，因为tree并没有个数的峰值。
+     因此minimum应该不赋值，找到第一个有效值的时候加以判断是否初始化，没有初始化则赋值。
+     如果minimum必须初始化，则是个永远不会出现的数比如-1或者null作为‘有待赋值’的标志
+     */
     public static int minDepthByDFS(TreeNode root) {
-        if (root == null) { return 0; }
+        if (root == null) {
+            return 0;
+        }
         Stack<TreeNode> ar = new Stack<TreeNode>();
         Stack<Integer> height = new Stack<Integer>();
         int minDepth = -1;
@@ -25,13 +46,17 @@ public class Tree111MinimumDepthOfBinaryTree {
         while (!ar.isEmpty()) {
             TreeNode currTN = ar.pop();
             Integer currHeight = height.pop();
-            if (currTN == null) { continue; }
+            if (currTN == null) {
+                continue;
+            }
             //Deal with the leaf
             if (currTN.left == null && currTN.right == null) {
                 if (minDepth == -1) {
                     minDepth = currHeight;
                 } else {
                     minDepth = Math.min(minDepth, currHeight);
+                    //break; 为什么这里不能break? 因为用的DFS方法, 必须traverse完所有节点, 比如最小depth在右子树,
+                    //而最先发现的叶子节点在左子树, 不一定是最小的. 如果break一定出错.
                 }
             } else {
                 ar.push(currTN.left); height.push(currHeight + 1);
@@ -41,9 +66,21 @@ public class Tree111MinimumDepthOfBinaryTree {
         return minDepth;
 
     }
-    //Runtime: 3ms Use: 40min optimized from IterativeDFS
-    //Use only 1 while loop and 1 queue:
-    //https://discuss.leetcode.com/topic/1948/my-solution-used-level-order-traversal
+    /**
+     * Runtime: 3ms Use: 40min optimized from IterativeDFS
+     //Use only 1 while loop and 1 queue:
+     //https://discuss.leetcode.com/topic/1948/my-solution-used-level-order-traversal
+
+     Trick1: BFS相比于DFS可以简化的地方
+
+     https://discuss.leetcode.com/topic/1948/my-solution-used-level-order-traversal
+     思路：
+     因为depth变成了常量，因此只能每一层traverse一遍后加1
+     而判断level结束，除了之前获取的本层size() 以外还有一个方法，就是设一个特殊值在每个level后面。这样每当==特殊值，并且queue里面还有别的tn，说明本层结束，下一层待traverse。就depth++
+     level-order traversal and record current level depth, when meet a node which both child is null then return, no need to go farther再次是因为从左到右按顺序，遇到的第一个左右子树为空的节点必定为minimumDepth，因此直接return就可以了，不需要往下进行。
+     细节处理请看Tree111的maxDepthByBFS()
+
+     */
     public static int minDepthByBFS(TreeNode root) {
         if (root == null) return 0;
         Queue<TreeNode> q = new ArrayDeque<TreeNode>();
@@ -67,7 +104,32 @@ public class Tree111MinimumDepthOfBinaryTree {
         }
         return dep;
     }
-
+    /**
+     * 6/12/2017 九章答案版本
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int min = helper(root);
+        return min;
+    }
+    private int helper(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MAX_VALUE;
+        if (root.left != null) {
+            left = helper(root.left);
+        }
+        if (root.right != null) {
+            right = helper(root.right);
+        }
+        return Math.min(left, right) + 1;
+    }
 
     public static void main(String[] args) {
         TreeNode a = new TreeNode(1);
