51dc5e23d2030e287462b65985ff77e402e3b4f6
YuqingLethe <yyq2983026@hotmail.com>
2017-06-29 14:16:04 -0400

    .

---
+++ Tree/Tree67BinaryTreeInorderTraversal.java
@@ -0,0 +1,30 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+public class Tree67BinaryTreeInorderTraversal {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack =  new Stack<TreeNode>();
+        TreeNode tn = root;
+        while (tn != null || !stack.empty()) {//是或啊!
+            //6/29/2017 又不会写了.....  注意这里加入和指针移动的先后
+            while (tn != null) {//一定要记得先push自己, 而不是左边的
+                stack.push(tn);
+                tn = tn.left;
+            }
+            tn = stack.pop();
+            result.add(tn.val);
+            tn = tn.right;
+        }
+        return result;
+    }
+}
