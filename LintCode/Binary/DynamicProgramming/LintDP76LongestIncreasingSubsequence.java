ca7e2e89395e348937f291d6e363c9a4511b7253
YuqingLethe <yyq2983026@hotmail.com>
2017-07-17 19:27:22 -0400

    .

--- LintCode/Binary/DynamicProgramming/LintDP76LongestIncreasingSubsequence.java
+++ LintCode/Binary/DynamicProgramming/LintDP76LongestIncreasingSubsequence.java
@@ -6,52 +6,34 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by Administrator on .
+ * Created by Administrator on .2017/7/17
  */
 public class LintDP76LongestIncreasingSubsequence {
     /**
-     * 2017/7/17 自己没想到要用这么多辅助数组, 但存储的东西多, 的确需要这么干, 要不就resulttype
+     * 2017/7/17
+     * normal DP solutions
      */
-    public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
+    public int longestIncreasingSubsequence(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         int len = nums.length;
-        int[] pre = new int[len]; //track the previous factor
-        int[] f = new int[len]; //record the length of current subset
-        f[0] = 1;
-        pre[0] = -1;
-        for (int i = 1; i < len; i++) {
+        int[] f = new int[len];
+        for (int i = 0; i < len; i++) {
             f[i] = 1;
-            pre[i] = -1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[j] == nums[i]) {
-                    continue;
-                }
-                if (nums[i] % nums[j] == 0) {
-                    pre[i] = j;
-                    f[i] = f[j] + 1;
-                    break; //遇到最大因子就停止, 因此应该break
-                }
-//                System.out.println("-- j=" + j + " f[j]=" + f[j]);
-            }
-//            System.out.println("i=" + i + " f[i]=" + f[i] + " pre[i]=" + pre[i]);
         }
-        //find the largest subset
-        int startIdx = -1;
-        int max = 0;
-        for (int i = len - 1; i >= 0; i--) {
-            if (max < f[i]) {
-                max = f[i];
-                startIdx = i;
+        int ans = 1;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[j] > nums[i] && f[j] <= f[i]) {
+                    f[j] = f[i] + 1;
+
+                }
+            }
+            if (f[i] > ans) { //写在外循环比较清楚. 外循环找到了截止到当前i的最大值
+                ans = f[i];
             }
         }
-        //insert into the answer list
-        List<Integer> ans = new ArrayList<>();
-        while(startIdx >= 0) {
-            ans.add(nums[startIdx]);
-            startIdx = pre[startIdx];
-        }
-        Collections.reverse(ans);
         return ans;
     }
-
 }
