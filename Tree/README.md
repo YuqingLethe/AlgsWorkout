9239bebe391e76945aaaf5b7e23a84920937ecc7
MiDo <yyq2983026@hotmail.com>
2020-03-16 20:29:16 -0400

    20200316 Use Stack in tree traverse

---
+++ Tree/README.md
@@ -0,0 +1,19 @@
+# Some quick notes and insights
+This is personal notes. Please do not see or use in any circumstances.
+
+## Tree112 PathSum
+针对Stack的活用并没想清楚，以后需要再做！
+
+
+## Memo Table
+Value level:
+* 1 Too easy
+* 3 Best to practice multi times and groom logic. Should do it several times in each round. You may have new thoughts every time.
+* 5 Too complex. Only do to activate the mind before interview.
+
+
+| Number | Problem | Difficulty| Value Level | Node |
+|------|--------|----------|-------------|-------------|
+| 112 | PathSum | Use Stack | 3 | Use stack in iterative Tree traverse, when to stop, where to stop
+| 1/2 | 5/22 | String | Top level build using Gradle Wrapper |
+| 1/3 | 5/24 | Sort | Push to Octopus Deployment using RestAPI |
--- Tree/Tree112PathSum.java
+++ Tree/Tree112PathSum.java
@@ -47,21 +47,27 @@ public class Tree112PathSum {
     /**
      * My Original Thoughts
      * https://discuss.leetcode.com/topic/59924/non-recursive-using-two-stacks-java
-     * @param root
-     * @param sum
-     * @return
+     *
+     * 20200316又没写出来这个。写出来的思路有:
+     * 1. 先判断叶子节点左右都是null的条件
+     * 2. 需要一个TreeNode Stack来帮助trace父节点
+     * 难点(没想明白的）
+     * 1. 怎样得知我们已经遍历了左节点还是右节点？还需要一个boolean stack来告诉我们吗？
+     *      答：并不用，因为binaryTree只有两个子树。只要把查看右节点和pop动作写在一起，
+     * 2. 什么是终结条件？
+     *     答： 终结条件和中间的memory设计有关。但主旨是treenode里面还有，说明还没遍历完，所以不结束。
+     * 3. 需要sum stack吗？还是只需要记录当前的值？
+     *     答：强烈建议用stack，因为不断地减加容易乱，不如和node stack一起pop push看得明白。
      */
     public boolean hasPathSumByTwoStack(TreeNode root, int sum) {
         return false;
+
     }
 
     /**
      * Runtime:  1ms  Use  5min  10/7/2016
      * Self-recursive method, CHANGE VAL of each treenode as the cumulative vals of the path
      * https://discuss.leetcode.com/topic/57216/java-solution-using-recursive-1ms
-     * @param root
-     * @param sum
-     * @return
      */
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
@@ -80,5 +86,28 @@ public class Tree112PathSum {
             rightAns = hasPathSum(root.right, sum);
         }
         return leftAns || rightAns;
+    }
+
+    /**
+     * 这是2020年的想法，思路很顺：1. 先写root node的逻辑，currSum是Sum减去root.val，左右子树两个if
+     * 2. 再想leaf node的逻辑，如果当前val和sum相等
+     * 漏掉了：如果root == null这个逻辑。在我的left note里面其实不会有这个，但不排除整个空树。
+     */
+    public boolean hasPathSum2020(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (sum == root.val && root.left == null && root.right == null) {
+            return true;
+        }
+        TreeNode currNode = root;
+        int currSum = sum - currNode.val;
+
+        if (currNode.left != null && hasPathSum(currNode.left, currSum)) {
+            return true;
+        } else if (currNode.right != null && hasPathSum(currNode.right, currSum)) {
+            return true;
+        }
+        return false;
     }
 }
