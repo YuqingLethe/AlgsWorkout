326152268a3dc8e1470b98595167460277c77646
Yoki Yang <yoki.yang@ingenico.com>
2022-03-05 18:51:39 -0500

    March2022 BinaryTree死磕所有method都要練習一遍

--- Tree/Tree110BalancedBinaryTree.java
+++ Tree/Tree110BalancedBinaryTree.java
@@ -10,79 +10,113 @@ import java.util.LinkedList;
  */
 public class Tree110BalancedBinaryTree {
     //之前的错误思路: http://blog.csdn.net/eatbanli/article/details/52658847
-
-
-    /**
-     * 9/24/2016 & 6/13/2017 Run this method and AC first time
-     */
-    public boolean IsBalancedTwoRecursion(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        if ((Math.abs(height(root.left) - height(root.right)) <= 1)
-             && (IsBalancedTwoRecursion(root.left) && IsBalancedTwoRecursion(root.right))
+    class TwoRecursion {
+        /**
+         * March 2022
+         * 9/24/2016 & 6/13/2017
+         */
+        public boolean IsBalancedTwoRecursion(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            if ((Math.abs(height(root.left) - height(root.right)) <= 1)
+                    && (IsBalancedTwoRecursion(root.left) && IsBalancedTwoRecursion(root.right))
             ){
-            return true;
-        }
-        return false;
-    }
-    private int height(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return Math.max(height(root.left), height(root.right)) + 1;
-    }
-
-    //
-    //https://discuss.leetcode.com/topic/11007/java-solution-based-on-height-check-left-and-right-node-in-every-recursion-to-avoid-further-useless-search/4
-
-    /**
-     * Runtime: 1ms Passed First Time Yay!
-     * 这个方法比IsBalancedTwoRecursion() 好的地方是, 如果左子树不balance,不再比较右子树. 更快
-     * Comparing the subtree's height in the getDepth(). O(n) instead of O(n^2)
-     * Get the idea in "https://discuss.leetcode.com/topic/11007/java-solution-based-on-height-check-left-and-right-node-in-every-recursion-to-avoid-further-useless-search"
-     * @param root
-     * @return
-     */
-    public static boolean isBalancedBySubHeight(TreeNode root) {
-        if (root == null || (root != null && root.left == null && root.right == null)) {
-            return true;
-        }
-        int leftHeight = getDepth(root.left);
-        int rightHeight = getDepth(root.right);
-        if (
-                (leftHeight == -1 || rightHeight == -1)
-                ||
-                (Math.abs(leftHeight - rightHeight) > 1)
-            ) {
+                return true;
+            }
             return false;
-        }else {
-            return true;
         }
-
+        private int height(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
     }
-    private static int getDepth(TreeNode root) {
-        if (root == null) return 0;
-        int leftDepth = getDepth(root.left);
-        //If the left child is balanced, we search the other one
-        if (leftDepth != -1) {
-            int rightDepth = getDepth(root.right);
-            //If the right child is balanced
-            if (rightDepth != -1) {
-                return Math.abs(leftDepth - rightDepth) > 1 ?  -1 :  1 + Math.max(leftDepth, rightDepth);
+
+    static class oneRecursion {
+        //
+        //https://discuss.leetcode.com/topic/11007/java-solution-based-on-height-check-left-and-right-node-in-every-recursion-to-avoid-further-useless-search/4
+
+        /**
+         * Runtime: 1ms Passed First Time Yay!
+         * 这个方法比IsBalancedTwoRecursion() 好的地方是, 如果左子树不balance,不再比较右子树. 更快
+         * Comparing the subtree's height in the getDepth(). O(n) instead of O(n^2)
+         * Get the idea in "https://discuss.leetcode.com/topic/11007/java-solution-based-on-height-check-left-and-right-node-in-every-recursion-to-avoid-further-useless-search"
+         * @param root
+         * @return
+         */
+        public static boolean isBalancedBySubHeight(TreeNode root) {
+            if (root == null || (root != null && root.left == null && root.right == null)) {
+                return true;
+            }
+            int leftHeight = getDepth(root.left);
+            int rightHeight = getDepth(root.right);
+            if ((leftHeight == -1 || rightHeight == -1)
+                ||(Math.abs(leftHeight - rightHeight) > 1)
+                ) {
+                return false;
+            }else {
+                return true;
             }
         }
-        return -1;
+        private static int getDepth(TreeNode root) {
+            if (root == null) return 0;
+            int leftDepth = getDepth(root.left);
+            //If the left child is balanced, we search the other one
+            if (leftDepth != -1) {
+                int rightDepth = getDepth(root.right);
+                //If the right child is balanced
+                if (rightDepth != -1) {
+                    return Math.abs(leftDepth - rightDepth) > 1 ?  -1 :  1 + Math.max(leftDepth, rightDepth);
+                }
+            }
+            return -1;
+        }
+        public void main(String[] args) {
+            TreeNode a = new TreeNode(1);
+            TreeNode b = new TreeNode(2);
+            a.left = b;
+            System.out.println(isBalancedBySubHeight(a));
+        }
     }
 
-    //Method 3: http://www.jiuzhang.com/solutions/balanced-binary-tree/
+    /**
+     * March 2022
+     * Method 3: http://www.jiuzhang.com/solutions/balanced-binary-tree/
+     */
+    class resultType {
+        class ResultType {
+            public boolean isBalanced = false;
+            public Integer depth = 0;
+            public ResultType(boolean isBalanced, Integer depth) {
+                this.isBalanced = isBalanced;
+                this.depth = depth;
+            }
+        }
+        public boolean isBalanced(TreeNode root) {
+            return getSubtreeInfo(root).isBalanced;
+        }
 
-
-    public static void main(String[] args) {
-        TreeNode a = new TreeNode(1);
-        TreeNode b = new TreeNode(2);
-        a.left = b;
-        System.out.println(isBalancedBySubHeight(a));
+        public ResultType getSubtreeInfo(TreeNode root) {
+            if (root == null) {
+                return new ResultType(true, 0);
+            }
+            if (root.left == null && root.right == null) {
+                return new ResultType(true, 1);
+            }
+            ResultType left = getSubtreeInfo(root.left);
+            if (!left.isBalanced) {
+                return new ResultType(false, -1);
+            }
+            ResultType right = getSubtreeInfo(root.right);
+            if (!right.isBalanced) {
+                return new ResultType(false, -1);
+            }
+            if (Math.abs(left.depth - right.depth) > 1) {
+                return new ResultType(false, -1);
+            }
+            return new ResultType(true, Math.max(left.depth, right.depth) + 1);
+        }
     }
-
 }
